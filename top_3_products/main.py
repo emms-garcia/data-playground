85baf106fe4d1924209d51f0a5791261eef13281
Emmanuel Garcia <emmanuel.garcia.solis@gmail.com>
2025-02-27 21:00:44 -0600

    Sample job

---
+++ top_3_products/main.py
@@ -0,0 +1,46 @@
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import sum as spark_sum, rank
+
+
+def top_3_products(spark: SparkSession):
+    products_df = spark.read.csv("products.csv", header=True, inferSchema=True)
+    sales_df = spark.read.csv("sales.csv", header=True, inferSchema=True)
+    customers_df = spark.read.csv("customers.csv", header=True, inferSchema=True)
+    product_quantities_df = (
+        sales_df
+            .filter((sales_df.sale_date >= "2024-01-01") & (sales_df.sale_date < "2025-01-01"))
+            .groupBy(sales_df.product_id)
+            .agg(spark_sum(sales_df.quantity).alias("quantity"))
+    )
+    results_df = (
+        sales_df
+            .join(products_df, "product_id")
+            .join(customers_df, "customer_id")
+            .join(product_quantities_df, "product_id")
+            .filter(customers_df.country.isin(["Canada", "Mexico", "USA"]))
+            .select(
+                "country",
+                "product_name",
+                product_quantities_df.quantity.alias("quantity"),
+                rank().over(Window.partitionBy(customers_df.country).orderBy(product_quantities_df.quantity.desc())).alias("rank"),
+            )
+    )
+    results_df = (
+        results_df
+            .filter(results_df.rank <= 3)
+            .orderBy(results_df.country, results_df.rank.asc())
+    )
+    results_df.show()
+
+
+if __name__ == "__main__":
+    spark = (
+        SparkSession
+        .builder
+        .appName("Top3Products")
+        .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow")
+        .config("spark.executor.extraJavaOptions", "-Djava.security.manager=allow")
+        .getOrCreate()
+    )
+    top_3_products(spark)
+    spark.stop()
